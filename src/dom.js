c0c45bcc640f69ddcd662b5dbd0ecec19cd5a53c
zach4531 <zciatteo96@gmail.com>
2024-08-11 00:01:09 -0400

    Change layout

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,11 @@
+import Fetch from './fetch';
+
 export default class DOM {
   static createElement(type, content) {
     const element = document.createElement(type);
     element.textContent = content;
     return element;
   }
+
+  static loadProject(id) {}
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import DOM from './dom.js';
 
 const mainContainer = document.querySelector('#content');
 
-const projects = Fetch.getProject(1);
-
-const title = DOM.createElement('h2', projects.title);
+const project = Fetch.getProject(1);
+const todos = Fetch.getProjectTodos(project.id);
+const title = DOM.createElement('h2', project.title);
 mainContainer.appendChild(title);
+
+todos.forEach((todo) => {
+  const element = DOM.createElement('p', todo.title);
+  mainContainer.appendChild(element);
+});
